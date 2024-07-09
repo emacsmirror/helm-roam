b53b0f1b2691c05407b2879488e7b1ef0e89d98a
vhqr0 <zq_cmd@163.com>
2024-07-09 22:24:18 +0800

    license

--- helm-roam.el
+++ helm-roam.el
@@ -6,6 +6,20 @@
 ;; Keywords: org-mode, roam, helm, convenience
 ;; URL: https://github.com/vhqr0/helm-roam
 
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, see
+;; <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; Helm command for org-roam, list roam nodes, find node, insert link
 ;; of node, and capture to a exist or new node.  M-x helm-roam.
